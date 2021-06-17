58d5979fc399fa2d90f242f6668a122df3f627c8
Radora <61745485+Radora@users.noreply.github.com>
2021-06-17 15:19:36 +0200

    Add files via upload

---
+++ web-shop-spring/src/main/java/com/example/webshopspring/api/PersonController.java
@@ -0,0 +1,49 @@
+package com.example.webshopspring.api;
+
+import com.example.webshopspring.model.Person;
+import com.example.webshopspring.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+import java.util.UUID;
+
+@RequestMapping("api/v1/person")
+@RestController
+public class PersonController {
+
+    private final PersonService personService;
+
+    @Autowired
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
+
+    @PostMapping
+    public void addPerson(@Valid @NotNull @RequestBody Person person){
+        personService.addPerson(person);
+    }
+
+    @GetMapping
+    public List<Person> getAllPeople(){
+        return personService.getAllPeople();
+    }
+
+    @GetMapping(path = "{id}")
+    public Person getPersonById(@PathVariable("id") UUID id){
+        return personService.getPersonById(id).orElse(null);
+    }
+
+    @DeleteMapping(path = "{id}")
+    public void deletePersonById(@PathVariable("id") UUID id){
+        personService.deletePerson(id);
+    }
+
+    @PutMapping(path = "{id}")
+    public void updatePerson(@PathVariable("id") UUID id, @Valid @NotNull @RequestBody Person personToUpdate){
+        personService.updatePerson(id,personToUpdate);
+    }
+
+}
