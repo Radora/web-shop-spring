1e691d549367fd1e2a1e58d019f0740886214fcc
Radoslav <Rado@rado.local>
2021-06-29 14:19:00 +0200

    routes ready for first milestone

--- web-shop-spring/src/main/java/com/example/webshopspring/model/Category.java
+++ web-shop-spring/src/main/java/com/example/webshopspring/model/Category.java
@@ -2,13 +2,13 @@ package com.example.webshopspring.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public class Categories {
+public class Category {
 
     private final int id;
     private String name;
 
 
-    public Categories(int id, String name) {
+    public Category(int id, String name) {
         this.id = id;
         this.name = name;
     }
