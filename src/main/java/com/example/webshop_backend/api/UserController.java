d22fb300c6aca52fb156ecb114e46c14127f4e1b
mattesho <esho_matti@hotmail.com>
2022-03-09 10:42:42 +0100

    added more logic to updateUser (password encrypt)

--- src/main/java/com/example/webshop_backend/api/UserController.java
+++ src/main/java/com/example/webshop_backend/api/UserController.java
@@ -57,6 +57,7 @@ public class UserController {
         user.setFirstname(userEdited.getFirstname());
         user.setSurname(userEdited.getSurname());
         user.setUsername(userEdited.getUsername());
+        user.setPassword(userEdited.getPassword());
         user.setEmail(userEdited.getEmail());
         user.setAddress(userEdited.getAddress());
         User updatedUser = userService.updateUser(user);
